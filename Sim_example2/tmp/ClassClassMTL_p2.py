fcc92251896bc96ca06e60b7f928fa1d8af5022e
tsteelejohnson91 <32077125+tsteelejohnson91@users.noreply.github.com>
2021-10-06 15:52:52 -0400

    Delete Sim_example2 directory

--- Sim_example2/tmp/ClassClassMTL_p2.py
+++
@@ -1,3 +0,0 @@
-layer1=add_layer(xs,Fsc,hidden_feats,activation_function=tf.sigmoid,dropout_function=True,lambda1=lambda1, keep_prob1=kprob)
-layer2=add_layer(tf.concat([xs,layer1],1),Fsc+hidden_feats,hidden_feats,activation_function=tf.sigmoid,dropout_function=True,lambda1=lambda1, keep_prob1=kprob)
-layerF=add_layer(tf.concat([xs,layer1,layer2],1),Fsc+hidden_feats+hidden_feats,hidden_feats,activation_function=tf.sigmoid,dropout_function=True,lambda1=lambda1, keep_prob1=kprob)
