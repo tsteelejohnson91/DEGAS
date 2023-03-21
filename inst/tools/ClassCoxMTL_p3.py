9e91449ea59078d38006dad849e825bf3aba8418
tsteelejohnson91 <32077125+tsteelejohnson91@users.noreply.github.com>
2023-03-21 12:17:28 -0400

    fixing resampleGamma bug

--- inst/tools/ClassCoxMTL_p3.py
+++ inst/tools/ClassCoxMTL_p3.py
@@ -21,6 +21,10 @@ np.random.shuffle(train_pat)				# CHANGED 20201217
 np.random.shuffle(train_sc)					# CHANGED 20201217
 train_sc2 = train_sc[0:scbatch_sz]			# CHANGED
 train_pat2 = train_pat[0:patbatch_sz]		# CHANGED
+while np.sum(np.sum(np.squeeze(Ysc[train_sc2,:])>0,axis=0)<2) > 0:
+	train_sc = resample(50,Ysc,idx_sc)			# CHANGED
+	np.random.shuffle(train_sc)					# CHANGED 20201217
+	train_sc2 = train_sc[0:scbatch_sz]			# CHANGED
 resampleGammaXYsc = resample_mixGamma(np.squeeze(Xsc[train_sc2,:]),np.squeeze(Ysc[train_sc2,:]),list(range(scbatch_sz)),scbatch_sz,Lsc)       # CHANGED 20201217
 tensor_train = {xs: np.concatenate([resampleGammaXYsc[0],np.squeeze(Xpat[train_pat2,:])]), ys_sc: resampleGammaXYsc[1], r_pat: Rmatrix(survtime[train_pat2]), c_pat: np.squeeze(censor[train_pat2]), lsc: resampleGammaXYsc[1].shape[0], lpat: len(train_pat2), kprob: do_prc}
 init=tf.global_variables_initializer()
@@ -42,5 +46,9 @@ for i in range(train_steps + 1):
 		np.random.shuffle(train_pat)				# CHANGED
 		train_sc2 = train_sc[0:scbatch_sz]			# CHANGED
 		train_pat2 = train_pat[0:patbatch_sz]		# CHANGED
+		while np.sum(np.sum(np.squeeze(Ysc[train_sc2,:])>0,axis=0)<2) > 0:
+			train_sc = resample(50,Ysc,idx_sc)			# CHANGED
+			np.random.shuffle(train_sc)					# CHANGED 20201217
+			train_sc2 = train_sc[0:scbatch_sz]			# CHANGED
 		resampleGammaXYsc = resample_mixGamma(np.squeeze(Xsc[train_sc2,:]),np.squeeze(Ysc[train_sc2,:]),list(range(scbatch_sz)),scbatch_sz,Lsc)       # CHANGED 20201217
 		tensor_train = {xs: np.concatenate([resampleGammaXYsc[0],np.squeeze(Xpat[train_pat2,:])]), ys_sc: resampleGammaXYsc[1], r_pat: Rmatrix(survtime[train_pat2]), c_pat: np.squeeze(censor[train_pat2]), lsc: resampleGammaXYsc[1].shape[0], lpat: len(train_pat2), kprob: do_prc}
